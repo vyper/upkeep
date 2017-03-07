71cdffd5f75e41a3a29aa8d46813a01c33327c46
Leonardo Saraiva <vyper@maneh.org>
2017-03-06 22:20:14 -0300

    Add find user by provider and uid

--- lib/upkeep/repositories/user_repository.rb
+++ lib/upkeep/repositories/user_repository.rb
@@ -1,2 +1,5 @@
 class UserRepository < Hanami::Repository
+  def find_by_provider_and_uid(provider, uid)
+    users.where(provider: provider, uid: uid).first
+  end
 end
--- spec/upkeep/repositories/user_repository_spec.rb
+++ spec/upkeep/repositories/user_repository_spec.rb
@@ -1,2 +1,3 @@
 RSpec.describe UserRepository do
+  it '#find_by_provider_and_uid'
 end
